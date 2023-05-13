e76e52e29b3a05c81cc44d5a53d9d49f957bfb36
EssamEzi <essamaa119@gmail.com>
2023-05-14 00:25:28 +0300

    Equatable objects Is equivalent to hashCode

--- lib/movies/domain/entities/movie.dart
+++ lib/movies/domain/entities/movie.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-class Movie {
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable {
   final int id;
   final String title;
   final String backdropPath;
@@ -8,7 +10,7 @@ class Movie {
   final double voteAverage;
   final String releaseDate;
   final String originalLanguage;
-  Movie({
+  const Movie({
     required this.id,
     required this.title,
     required this.backdropPath,
@@ -18,4 +20,49 @@ class Movie {
     required this.releaseDate,
     required this.originalLanguage,
   });
+
+  // List<Object?> get props => throw UnimplementedError();
+  //نخلي نفس الكلاس متشابه
+  @override
+  List<Object> get props {
+    return [
+      id,
+      title,
+      backdropPath,
+      genreIds,
+      overview,
+      voteAverage,
+      releaseDate,
+      originalLanguage,
+    ];
+  }
+
+  //التاكد من ان الكائنات التي تتشابه في القيم اي لديها نفس القيم
+  // لا تحجز مكان اخر في الذاكره
+  // حيث يكون الهاش كود متشابه
+  //Equatableيمكن استخدام هذا الكود الو الحزمه التاليه
+  // @override
+  // bool operator ==(Object other) =>
+  //     identical(this, other) ||
+  //     other is Movie &&
+  //         runtimeType == other.runtimeType &&
+  //         id == other.id &&
+  //         title == other.title &&
+  //         backdropPath == other.backdropPath &&
+  //         genreIds == other.genreIds &&
+  //         overview == other.overview &&
+  //         voteAverage == other.voteAverage &&
+  //         releaseDate == other.releaseDate &&
+  //         originalLanguage == other.originalLanguage;
+
+  // @override
+  // int get hashCode =>
+  //     id.hashCode ^
+  //     title.hashCode ^
+  //     backdropPath.hashCode ^
+  //     genreIds.hashCode ^
+  //     overview.hashCode ^
+  //     voteAverage.hashCode ^
+  //     releaseDate.hashCode ^
+  //     originalLanguage.hashCode;
 }
