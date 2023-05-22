5243dfd13959f923391416259dd5fa4e27d6f270
EssamEzi <essamaa119@gmail.com>
2023-05-23 00:09:48 +0300

    BlocBuilder get Data NowPlaying  Screen

--- lib/movies/presentation/components/now_playing_component.dart
+++ lib/movies/presentation/components/now_playing_component.dart
@@ -2,8 +2,11 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_app/core/network/api_constance.dart';
-import 'package:movies_app/core/utils/dummy.dart';
+import 'package:movies_app/core/utils/enums.dart';
+import 'package:movies_app/movies/presentation/controller/movies_bloc.dart';
+import 'package:movies_app/movies/presentation/controller/movies_state.dart';
 
 class NowPlayingComponent extends StatelessWidget {
   const NowPlayingComponent({
@@ -12,96 +15,120 @@ class NowPlayingComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FadeIn(
-      //animate_do
-      //التي تحتوي على الصور الافلام sliderعمل انميشن لحركه
-      duration: const Duration(milliseconds: 500),
-      child: CarouselSlider(
-        options: CarouselOptions(
-          height: 400.0,
-          viewportFraction: 1.0,
-          onPageChanged: (index, reason) {},
-        ),
-        //عناصر slider
-        items: moviesList.map(
-          (item) {
-            return GestureDetector(
-              key: const Key('openMovieMinimalDetail'),
-              onTap: () {},
-              // النص يكون خلف الصوره
-              child: Stack(
-                children: [
-                  ShaderMask(
-                    //slider عمل الشادو من فوق وتحت
-                    shaderCallback: (rect) {
-                      return const LinearGradient(
-                        // اللوان الشادو
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          // fromLTRB
-                          Colors.transparent,
-                          Colors.black,
-                          Colors.black,
-                          Colors.transparent,
-                        ],
-                        stops: [0, 0.3, 0.5, 1],
-                      ).createShader(
-                        Rect.fromLTRB(0, 0, rect.width, rect.height),
-                      );
-                    },
-                    blendMode: BlendMode.dstIn,
-                    //الصوره
-                    child: CachedNetworkImage(
-                      height: 560.0,
-                      imageUrl: ApiConstance.imageUrl(item.backdropPath),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 16.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Icon(
-                                Icons.circle,
-                                color: Colors.redAccent,
-                                size: 16.0,
-                              ),
-                              const SizedBox(width: 4.0),
-                              Text(
-                                'Now Playing'.toUpperCase(),
-                                style: const TextStyle(
-                                  fontSize: 16.0,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 16.0),
-                          child: Text(
-                            item.title,
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              fontSize: 24,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+    return BlocBuilder<MoviesBloc, MoviesState>(
+      builder: (context, state) {
+        print('BlocBuilder NowPlayingComponent');
+        switch (state.nowPlayingState) {
+          case RequestState.loading:
+            return const SizedBox(
+              height: 400.0,
+              child: Center(
+                child: CircularProgressIndicator(),
               ),
             );
-          },
-        ).toList(),
-      ),
+          case RequestState.loaded:
+            return FadeIn(
+              //animate_do
+              //التي تحتوي على الصور الافلام sliderعمل انميشن لحركه
+              duration: const Duration(milliseconds: 500),
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  height: 400.0,
+                  viewportFraction: 1.0,
+                  onPageChanged: (index, reason) {},
+                ),
+                //عناصر slider
+                items: state.nowPlayingMovies.map(
+                  (item) {
+                    return GestureDetector(
+                      key: const Key('openMovieMinimalDetail'),
+                      onTap: () {},
+                      // النص يكون خلف الصوره
+                      child: Stack(
+                        children: [
+                          ShaderMask(
+                            //slider عمل الشادو من فوق وتحت
+                            shaderCallback: (rect) {
+                              return const LinearGradient(
+                                // اللوان الشادو
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [
+                                  // fromLTRB
+                                  Colors.transparent,
+                                  Colors.black,
+                                  Colors.black,
+                                  Colors.transparent,
+                                ],
+                                stops: [0, 0.3, 0.5, 1],
+                              ).createShader(
+                                Rect.fromLTRB(0, 0, rect.width, rect.height),
+                              );
+                            },
+                            blendMode: BlendMode.dstIn,
+                            //الصوره
+                            child: CachedNetworkImage(
+                              height: 560.0,
+                              imageUrl:
+                                  ApiConstance.imageUrl(item.backdropPath),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 16.0),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      const Icon(
+                                        Icons.circle,
+                                        color: Colors.redAccent,
+                                        size: 16.0,
+                                      ),
+                                      const SizedBox(width: 4.0),
+                                      Text(
+                                        'Now Playing'.toUpperCase(),
+                                        style: const TextStyle(
+                                          fontSize: 16.0,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 16.0),
+                                  child: Text(
+                                    item.title,
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                      fontSize: 24,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ).toList(),
+              ),
+            );
+
+          case RequestState.error:
+            return SizedBox(
+              height: 400,
+              child: Center(
+                child: Text(state.nowPlayingMessage),
+              ),
+            );
+        }
+      },
     );
   }
 }
--- lib/movies/presentation/screens/movies_screen.dart
+++ lib/movies/presentation/screens/movies_screen.dart
@@ -6,31 +6,31 @@ import 'package:movies_app/movies/presentation/components/now_playing_component.
 import 'package:movies_app/movies/presentation/components/popular_component.dart';
 import 'package:movies_app/movies/presentation/components/top_rated_component.dart';
 import 'package:movies_app/movies/presentation/controller/movies_bloc.dart';
+import 'package:movies_app/movies/presentation/controller/movies_event.dart';
 
 class MainMoviesScreen extends StatelessWidget {
   const MainMoviesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    print('build');
     return BlocProvider(
-      create: (context) => sl<MoviesBloc>(),
+      //اضافة الاحداث البلوكٍ
+      create: (context) => sl<MoviesBloc>()..add(GetNowPlayingMoviesEvent()),
       child: Scaffold(
-        body: SafeArea(
-          child: SingleChildScrollView(
-            key: const Key('movieScrollView'),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const NowPlayingComponent(),
-                CustomContainerMoviesScreen(
-                    textPopular: 'Popular', onTap: () {}),
-                const PopularComponent(),
-                CustomContainerMoviesScreen(
-                    textPopular: 'Top Rated', onTap: () {}),
-                const TopRatedComponent(),
-                const SizedBox(height: 50.0),
-              ],
-            ),
+        body: SingleChildScrollView(
+          key: const Key('movieScrollView'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const NowPlayingComponent(),
+              CustomContainerMoviesScreen(textPopular: 'Popular', onTap: () {}),
+              const PopularComponent(),
+              CustomContainerMoviesScreen(
+                  textPopular: 'Top Rated', onTap: () {}),
+              const TopRatedComponent(),
+              const SizedBox(height: 50.0),
+            ],
           ),
         ),
       ),
