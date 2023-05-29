7f41e969020cba05d99ef464d8d06ca203a902e4
EssamEzi <essamaa119@gmail.com>
2023-05-30 00:52:29 +0300

    model Data layer movie detail and genres

---
+++ lib/movies/data/model/movie_detalis_model.dart
@@ -0,0 +1,28 @@
+import 'package:movies_app/movies/data/model/genres_model.dart';
+import 'package:movies_app/movies/domain/entities/movie_detail.dart';
+
+class MovieDetailsModel extends MovieDetail {
+  const MovieDetailsModel(
+      {required super.backdropPath,
+      required super.id,
+      required super.genres,
+      required super.overview,
+      required super.releaseDate,
+      required super.runtime,
+      required super.title,
+      required super.voteAverage});
+  factory MovieDetailsModel.fromJson(Map<String, dynamic> json) =>
+      MovieDetailsModel(
+          backdropPath: json['backdrop_path'],
+          id: json['id'],
+          genres: List<GenresModel>.from(
+            json['genres'].map(
+              (elements) => GenresModel.fromJson(elements),
+            ),
+          ),
+          overview: json['overview'],
+          releaseDate: json['release_date'],
+          runtime: json['runtime'],
+          title: json['title'],
+          voteAverage: json['vote_average']);
+}
